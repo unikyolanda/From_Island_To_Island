878c77c113fb3875521ce95f18e7221d194aaa26
Yolanda <yolanda@chengchitech.com>
2025-02-05 10:48:35 +0800

    🐛 [fix] opacity

--- components/SideMenu.vue
+++ components/SideMenu.vue
@@ -15,7 +15,7 @@
             <p class="font-shippori text-[28px] tracking-[4px]">由島至島</p>
             <p class="font-wix text-lg tracking-[4px]">FILM</p>
           </div>
-          <div class="flex flex-col items-start space-y-5 w-1/2">
+          <div class="flex flex-col items-start space-y-5 w-1/2 ml-8">
             <a href="#" class="font-noto text-lg tracking-[3px]">電影介紹</a>
             <a href="#" class="font-noto text-lg tracking-[3px]">座談與報導</a>
             <a href="#" class="font-noto text-lg tracking-[3px]">包場資訊</a>
@@ -26,7 +26,7 @@
             <p class="font-shippori text-[28px] tracking-[4px]">主題教案</p>
             <p class="font-wix text-lg tracking-[4px]">LESSON</p>
           </div>
-          <div class="flex flex-col items-start space-y-5 w-1/2">
+          <div class="flex flex-col items-start space-y-5 w-1/2 ml-8">
             <a href="#" class="font-noto text-lg tracking-[3px]">移動與邊界</a>
             <a href="#" class="font-noto text-lg tracking-[3px]">加害與被害</a>
             <a href="#" class="font-noto text-lg tracking-[3px]">共犯結構</a>
@@ -34,12 +34,17 @@
             <a href="#" class="font-noto text-lg tracking-[3px]">為什麼要記憶</a>
           </div>
         </div>
-        <div class="flex flex-col items-end">
-          <p class="font-noto text-2xl tracking-[4px]">聯絡我們</p>
-          <p class="font-amiri italic text-lg">CONTACT</p>
+        <div class="flex justify-between w-full items-center">
+          <div class="flex flex-col items-center w-1/2">
+            <p class="font-shippori text-[28px] tracking-[4px]">聯絡我們</p>
+            <p class="font-wix text-lg tracking-[4px]">CONTACT</p>
+          </div>
+          <div class="flex flex-col items-start space-y-5 w-1/2 ml-8">
+            <a href="#" class="font-noto text-lg tracking-[3px]">捐款支持</a>
+          </div>
         </div>
         <div class="flex flex-col items-end space-y-4">
-          <a href="#" class="font-noto tracking-[2px]">捐款支持</a>
+          <a href="#" class="font-noto tracking-[2px]"></a>
         </div>
       </nav>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -111,7 +111,7 @@
           if (direction === 1) {
             // Scrolling down
             gsap.to(text, {
-              opacity: 1 - progress,
+              opacity: progress * 1.5,
               y: progress * -30,
               duration: 1.25,
               delay: delay,
@@ -265,7 +265,9 @@
         <p class="font-noto text-white tracking-[2px] w-[412px] leading-[2]">
           台灣在二戰期間是日本帝國的一部分。本片以此為主題，努力挖掘和重新整理台灣在二戰期間的隱藏記憶，探索在日本帝國內的台灣士兵、醫生與生活在東南亞的海外台灣人之經歷。全片從兒子對父親的提問開場，透過跨世代的對話、家書、日記和家庭成員之間的影片，探討台灣歷史記憶的複雜性，以及這一時期存在的各種身分。
         </p>
-        <button class="font-wix text-white text-lg border border-white py-4 w-[162px]">
+        <button
+          class="font-wix text-white text-lg border border-white py-4 w-[162px] hover:bg-white hover:text-[#aaa]"
+        >
           View more
         </button>
       </div>
@@ -290,7 +292,9 @@
         <p class="font-noto text-white tracking-[2px] w-[412px] leading-[2]">
           《由島至島》教育推廣規劃，從本片中節選出５個主題的片段，發展搭配教案，免費開放老師申請作為課程教授素材。五大主題為：移動與邊界、加害與被害、共犯結構－日本擴張行動、克服過去、為什麼要記憶，各有台灣、日本及馬來西亞教案可下載。
         </p>
-        <button class="font-wix text-white text-lg border border-white py-4 w-[162px]">
+        <button
+          class="font-wix text-white text-lg border border-white py-4 w-[162px] hover:bg-white hover:text-[#aaa]"
+        >
           View more
         </button>
       </div>
